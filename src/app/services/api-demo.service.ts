72ea134f51f98c939394fe84053f081f9e5abc20
Unhale <pankaj.unhale@capgemini.com>
2019-08-12 15:51:16 +0530

    code cleanup

--- src/app/services/api-demo.service.ts
+++ src/app/services/api-demo.service.ts
@@ -6,13 +6,27 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class ApiDemoService {
-
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   constructor(private _http: HttpClient) {
     this.login();
    }
 
-  public login(){
+  public loginGet(){
     this._http.get(environment.baseUrl+'/adLogin?q=pankajunhale').subscribe((response:any)=>{
+      console.log(response);      
+    },(error:any)=>{
+      console.log(error);      
+    })
+  }
+
+  public login(){
+    let data = {
+      name:'pankaj',
+      job:'temp'
+    }
+    this._http.post(environment.baseUrl+'/create',JSON.stringify(data)).subscribe((response:any)=>{
       console.log(response);      
     },(error:any)=>{
       console.log(error);      
